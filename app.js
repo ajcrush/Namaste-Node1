2b85661b91d5ce94350a3c8c41ea1ca3bca03418
Mohit sharma <mohitanand8987@gmail.com>
2024-09-24 20:53:40 +0530

    hello

---
+++ app.js
@@ -0,0 +1,9 @@
+require("./xyz.js");
+const calculateSum = require("./sum.js");
+var name1 = "Namaste NodeJS";
+var a = 10;
+var b = 20;
+console.log(name1);
+calculateSum(a, b);
+console.log(a + b);
+console.log(this);
