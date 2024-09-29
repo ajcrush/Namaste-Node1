39904a23f0eea965a24d49d53bbfe79d0e6d185d
Mohit sharma <mohitanand8987@gmail.com>
2024-09-29 15:37:00 +0530

    episode 3 module export require

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require("./xyz.js");
 const { x, calculateSum } = require("./sum.js");
+// import { calculateSum } from "./sum.js";
 var name1 = "Namaste NodeJS";
 var a = 10;
 var b = 20;
 console.log(name1);
 calculateSum(a, b);
-console.log(x);
+//console.log(x);
 console.log(a + b);
 console.log(this);
