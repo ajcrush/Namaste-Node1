b58e19b5996a33b72662dfe7ee570efd0200ce4d
Mohit sharma <mohitanand8987@gmail.com>
2024-10-26 22:17:04 +0530

    created a basic server

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const http = require("http");
-const server = http.createServer();
+const server = http.createServer(function (req, res) {
+  // Reply to them with hello world
+  if (req.url === "/getSecretData") {
+    res.end("No secret data");
+  }
+  res.end("Hello world");
+});
+server.listen(7777);
